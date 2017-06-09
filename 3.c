572ab05da1f5ca149d1a65eb443543a2b2fc32a6
santosde <doctsodesousa@gmail.com>
2017-06-09 05:18:44 +0300

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+     float R1, R2, R;
+     printf("пиште первое число\n");
+     scanf("%f", &R1);
+      printf("пишите второе число\n");
+      scanf("%f", &R2);
+      R=(R1*R2)/(R1+R2);
+      printf("R=%f\n", R);
+      return 0;
+}
