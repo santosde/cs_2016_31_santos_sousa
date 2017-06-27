7b5d736f46bb93d4d59eafcbf894adc99534b3a9
santosde <doctsodesousa@gmail.com>
2017-06-27 19:50:14 +0300

    Create 20.C

---
+++ Module 1/20.C
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "conio.h"
+#include "string.h"
+
+char *Encrypt(char msg[256], int n);
+char *Decrypt(char msg[256], int n);
+void stop(void);
+
+void main() {
+  char msg[256];
+  int n;
+  while (1) {
+    clrscr();
+    printf("Enter a text, (FIM) to finish:\n");
+   fflush(stdin); 
+    gets(msg);
+    if (strcmp(msg, "FIM") == 0)
+       break;
+    printf("Enter the order of the digit:\n");
+    scanf("%d", &n);
+
+    char *Encrypted = encrypted(msg, n);
+    printf("\nEncrypted Message = %s", Coded);
+
+    char *Normal = decrypt(coded, n);
+    printf("\nEncrypted Message = %s", normal);
+    stop();
+  }
+}
+char * encrypt(char msg[256], int n) {
+ for (int i=0; i<strlen(msg); i++)
+    msg[i] = msg[i] + n;
+
+  return(msg);
+}
+
+char *decrypt(char msg[256], int n) {
+  for (int i=0; i<strlen(msg); i++)
+    msg[i] = msg[i] - n;
+
+  return(msg);
+  }
+
+void stop(void) {
+  gotoxy(01, 24);
+  printf("Press [something] to continue.");
+  getch();
+}
